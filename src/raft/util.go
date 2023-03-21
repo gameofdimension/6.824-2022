8b0f1ba7b934216d056fb2ecea34c32ba30828c9
felixdae <felixdae@gmail.com>
2023-03-21 18:51:39 +0800

    refactor

--- src/raft/util.go
+++ src/raft/util.go
@@ -11,3 +11,13 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	}
 	return
 }
+
+func isMoreUpToDate(aTerm int, aIndex int, bTerm int, bIndex int) bool {
+	if aTerm > bTerm {
+		return true
+	}
+	if aTerm < bTerm {
+		return false
+	}
+	return aIndex >= bIndex
+}
