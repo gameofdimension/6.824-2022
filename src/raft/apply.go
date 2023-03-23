b9b6b3f57a87f59a83176da03e8821d914b9b494
felixdae <felixdae@gmail.com>
2023-03-23 10:29:42 +0800

    add apply

---
+++ src/raft/apply.go
@@ -0,0 +1,27 @@
+package raft
+
+import "time"
+
+func (rf *Raft) applyLog() {
+	for {
+		rf.mu.Lock()
+		idle := true
+		if rf.commitIndex > rf.lastApplied {
+			rf.lastApplied += 1
+			rf.applyCh <- ApplyMsg{
+				CommandValid: true,
+				Command:      rf.log[rf.lastApplied].Command,
+				CommandIndex: rf.lastApplied,
+			}
+			idle = false
+		}
+		rf.mu.Unlock()
+		if idle {
+			time.Sleep(10 * time.Millisecond)
+		}
+	}
+}
+
+func (rf *Raft) spawnWorker() {
+	go rf.applyLog()
+}
